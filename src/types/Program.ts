3b3bfc79381202a90338235b20c41bfb002e4077
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-12-02 17:54:59 -0300

    criado o arquivo com types

---
+++ src/types/Program.ts
@@ -0,0 +1,11 @@
+export interface Program {
+    id: number;
+    title: string;
+    description?: string;
+    name: string;
+    image?: {
+        medium: string;
+        original: string;
+    } | null;
+    genres:string
+}
