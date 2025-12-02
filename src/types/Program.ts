a79fd4b55ed01c77e8f035111456a01ae88712cc
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-12-02 18:39:25 -0300

    adicionado novos types

--- src/types/Program.ts
+++ src/types/Program.ts
@@ -7,5 +7,8 @@ export interface Program {
         medium: string;
         original: string;
     } | null;
-    genres:string
+    genres: string;
+    rating: {
+        average: string;
+    }
 }
