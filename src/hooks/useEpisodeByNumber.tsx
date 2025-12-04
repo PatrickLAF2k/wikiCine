2debeb18967ea4a6749f1867e4dcbb990e7b4751
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-12-04 17:07:14 -0300

    Criado o hook para o episodio por numero

---
+++ src/hooks/useEpisodeByNumber.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { getEpisodeByNumber } from "../services/showService";
+
+export function useEpisodeByNumber(showId: number, season: number, number: number) {
+    const [episode, setEpisode] = useState<any>(null); // ou crie uma interface tipo Episode
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        async function load() {
+            try {
+                setLoading(true);
+                setError(null);
+                const data = await getEpisodeByNumber(showId, season, number);
+                setEpisode(data);
+            } catch (err: any) {
+                setError(err.message || "Erro ao buscar episódio");
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        load();
+    }, [showId, season, number]);
+
+    return { episode, loading, error };
+}
