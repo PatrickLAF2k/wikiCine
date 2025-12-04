5cfd138bee32d8873ec1ba5e4a058fd735f8f488
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-12-04 03:30:29 -0300

    movida a API para arquivo unico junto com as chamadas

--- src/services/showService.ts
+++ src/services/showService.ts
@@ -1,6 +1,27 @@
-import { api } from "./api";
+import axios from "axios";
+
+const API_BASE_URL = 'https://api.tvmaze.com';
+
+const api = axios.create({
+    baseURL: API_BASE_URL,
+});
 
 export async function getAllShows(page: number) {
     const response = await api.get(`/shows?page=${page}`);
+    return response.data;
+}
+
+export async function searchShowsByName(name: string) {
+    const response = await api.get(`/search/shows?q=${encodeURIComponent(name)}`);
+    return response.data.map((item: any) => item.show);
+}
+
+export async function getShowById(id: number) {
+    const response = await api.get(`/shows/${id}`);
+    return response.data;
+}
+
+export async function getEpisodesByShowId(id: number) {
+    const response = await api.get(`/shows/${id}/episodes`);
     return response.data;
 }
