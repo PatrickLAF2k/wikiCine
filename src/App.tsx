93251372dbc0fc3f9bc505581ef2111db8b630d5
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-12-04 03:25:26 -0300

    Configurado os caminhos das paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
-import Home from './pages/Home'
+import HomePage from './pages/HomePage'
+import DetailsPage from './pages/DetailsPage'
+
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/series/:id" element={<DetailsPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
