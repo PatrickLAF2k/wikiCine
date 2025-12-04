22e0a6a4c6496d4ed436791de1d1403fb34116cc
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-12-04 03:30:04 -0300

    alterado o nome de Home para HomePage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,55 @@
+import {
+    PaginationContent,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination"
+import { Spinner } from "@/components/ui/spinner"
+
+import SeriesCard from "@/components/SeriesCard";
+import { useShows } from "../hooks/useShows";
+import { useState } from "react";
+import ShowSearch from "@/components/SeriesSearch";
+
+export default function HomePage() {
+    const [page, setPage] = useState(0);
+    const { shows, loading } = useShows(page);
+
+
+    return (
+        <>
+            <ShowSearch />
+
+            <main >
+                <div className="flex  justify-between container mx-auto p-5">
+
+                    <h2 className="text-lg font-bold ">
+                        Todos os programas
+                    </h2>
+
+                    <PaginationContent className="w-1/2 justify-end">
+                        <PaginationPrevious href="#" onClick={(e) => { e.preventDefault(); if (page > 0) setPage(page - 1); }} />
+
+                        <p>{page}</p>
+
+                        <PaginationNext href="#" onClick={(e) => { e.preventDefault(); setPage(page + 1); }} />
+                    </PaginationContent>
+
+                </div>
+
+
+                {loading && <Spinner />}
+
+                {!loading && (
+                    <div className=" container mx-auto py-5 px-3  grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-5" >
+
+                        {shows.map((show: Program) => (
+                            <SeriesCard key={show.id} show={show} />
+
+                        ))}
+                    </div>
+                )
+                }
+            </main >
+        </>
+    );
+}
