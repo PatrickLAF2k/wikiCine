b3f4ce97cd2b227aada0c4f2c5e589d5e0352795
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-12-04 03:26:22 -0300

    oraganizado o header para utilizar os links das rotas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,39 @@
+import { Link } from "react-router-dom"
+import { Button } from "@/components/ui/button"
 import { Icon } from "@iconify/react"
 
 
 export default function Header() {
 
   return (
-    <header className="flex justify-evenly border-b border-border bg-card">
 
-      <div className="flex items-center gap-2 text-xl font-bold" >
-        <Icon icon="ri:movie-2-ai-line" fontSize="3em" />
-        <h2>Wiki Cine</h2>
+    <header className="border-b bg-background/35 backdrop-blur  sticky top-0 z-50">
+
+      <div className="flex justify-between container mx-auto px-4 py-4">
+
+        <Link to="">
+          <nav className="flex items-center ">
+            <Icon icon="ri:movie-2-ai-line" fontSize="3em" />
+            <h1 className="" >Wiki Cine</h1>
+          </nav>
+        </Link>
+
+
+
+        <div className="flex items-center gap-2">
+
+          <Link to="">
+            <Button > Programas </Button>
+          </Link>
+
+
+
+          <Link to="/favorites">
+            <Button> Favoritos <Icon icon="tabler:hearts" width="24" height="24" color="red" /> </Button>
+          </Link>
+
+        </div>
       </div>
-
-      <nav className="flex justify-between items-center gap-2 text-xl font-bold w-1/3 ">
-        <a href="">Series</a>
-        <a href="">Animes</a>
-      </nav>
-
     </header>
   )
 }
-
