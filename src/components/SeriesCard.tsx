a783ac671c8832d7aecb2d872987f1bd014241a8
PatrickLAF2k <patrickleonardoaf@gmail.com>
2025-12-04 03:26:40 -0300

    criado o componente para os cards das series

---
+++ src/components/SeriesCard.tsx
@@ -0,0 +1,39 @@
+import { Icon } from "@iconify/react";
+import { useNavigate } from "react-router-dom";
+import { Badge } from "@/components/ui/badge"
+
+export default function EpisodesCard({ show }: Program) {
+    const navigate = useNavigate();
+
+    function handleClick() {
+        navigate(`/series/${show.id}`);
+
+    }
+
+    return (
+        <div className="relative p-3 border rounded-lg shadow-md  transition-all duration-300 hover:shadow-xl hover:scale-105 cursor-pointer"
+            onClick={handleClick}>
+
+            {show.image?.medium != null ? (
+                <img
+                    src={show.image.medium}
+                    alt={show.name}
+                    className="w-full rounded-lg"
+                />
+            ) : (
+                <Icon icon="material-symbols-light:hide-image-outline" className="w-full rounded-lg border-2 h-86" />
+            )}
+
+            <Badge className=" flex items-center  absolute bottom-1/5  left-5 md:bottom-1/8 ">
+
+                <Icon icon="material-symbols:star-shine-outline-rounded" color="blue" />
+
+                <p> {show.rating.average != null ? show.rating.average.toFixed(1) : "Não avaliado"} </p>
+            </Badge>
+
+            <p className="font-semibold whitespace-nowrap overflow-hidden text-ellipsis py-1">{show.name}</p>
+
+        </div>
+    );
+}
+
